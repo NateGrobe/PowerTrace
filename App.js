ef6777687d4b8367ac382e308d112c4c23b70d64
Nate <nate.grobe@gmail.com>
2020-05-29 12:06:47 -0400

    started ui

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
-export default function App() {
+const App = () => {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Power Trace</Text>
+      <Text>Welcome Back!</Text>
+
+      <Button title='Bluetooth' />
+      <Button title='Location Services' />
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
