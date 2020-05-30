e9e32ebc9c064cf2ab6f36fe54879a96aa74e049
Raúl Chirinos <raulstuf@gmail.com>
2020-05-30 13:15:47 -0400

    Fix Dashboard merge conflics

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
+import axios from "axios";
 
 import ProgressBar from "../components/ProgressBar";
 import StatusBoard from "../components/StatusBoard";
 import PeopleIconsBar from "../components/PeopleIconsBar";
+
+import userServices from "../services/user";
 
 export default function DashboardPage() {
   const [mounted, setMounted] = useState(false);
@@ -16,40 +19,58 @@ export default function DashboardPage() {
 }
 
 const Dashboard = () => {
+  const [user, setUser] = useState([]);
+  const tmpUsr = "5ed15e8c32b5edd94dcdd00b";
+
+  useEffect(() => {
+    userServices.getUser(tmpUsr).then((res) => {
+      setUser(res);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          marginTop: 20,
-        }}
-      >
-        <ProgressBar style={styles.progressBar} />
+      <View style={{ paddingTop: 40 }}>
+        <View>
+          <ProgressBar style={styles.progressBar} />
+        </View>
       </View>
       <PeopleIconsBar days="76" style={{ marginBottom: 5 }} />
       <View style={styles.statusContainer}>
-        <View style={{ marginTop: 10, marginBottom: 15 }}>
+        <View style={{ paddingTop: 50 }}>
           <StatusBoard
             style={styles.statusBoard}
             boxTitle="My Health Status"
             boxDescription="Keep track of your health status and update it to keep those around
             you safe!"
             boxIndicator="Current Status"
-            boxContent="H E A L T H Y & V I R U S - F R E E 💪"
+            // boxContent="H E A L T H Y  &  V I R U S - F R E E 💪"
+            boxContent={
+              user.infected
+                ? "I N F E C T E D !"
+                : "H E A L T H Y & V I R U S - F R E E 💪"
+            }
             btnText="Update Status"
             iconName="heartbeat"
           />
         </View>
         <View>
-          <StatusBoard
-            style={styles.statusBoard}
-            boxTitle="My Risk Levels"
-            boxDescription="Stay informated of your possible exposure to the virus"
-            boxIndicator="Current Risk"
-            boxContent="S A F E ✔️"
-            btnText="Check your Exposure"
-            iconName="warning"
-            complementaryText="Great! You’ve not been in contact with COVID-19 patients. "
-          />
+          <View style={{ paddingTop: 50 }}>
+            <StatusBoard
+              style={styles.statusBoard}
+              boxTitle="My Risk Levels"
+              boxDescription="Stay informated of your possible exposure to the virus"
+              boxIndicator="Current Risk"
+              boxContent={user.infected ? "U N S A F E" : "S A F E ✔️"}
+              btnText="Check your Exposure"
+              iconName="warning"
+              complementaryText={
+                user.infected
+                  ? "Oh no! You have likely been infected."
+                  : "Great! You’ve not been in contact with COVID-19 patients. "
+              }
+            />
+          </View>
         </View>
       </View>
     </View>
