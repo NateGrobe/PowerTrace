e23a25d466eba3aec7888f5007719d6e67413685
Raúl Chirinos <raulstuf@gmail.com>
2020-05-29 23:56:31 -0400

    Create Progress Bar

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,13 +1,14 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import * as React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as React from "react";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import ReportScreen from '../screens/Report'
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
+import ReportScreen from "../screens/Report";
+import DashboardScreen from "../screens/DashboardScreen";
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = "Home";
 
 export default function BottomTabNavigator({ navigation, route }) {
   navigation.setOptions({ headerTitle: getHeaderTitle(route) });
@@ -18,29 +19,44 @@ export default function BottomTabNavigator({ navigation, route }) {
         name="Home"
         component={HomeScreen}
         options={{
-          title: 'Home',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-home" />,
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-home" />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Dashboard"
+        component={DashboardScreen}
+        options={{
+          title: "Dashboard",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-medkit" />
+          ),
         }}
       />
       <BottomTab.Screen
         name="Report"
         component={ReportScreen}
         options={{
-          title: 'Report',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-call" />,
+          title: "Report",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-call" />
+          ),
         }}
       />
     </BottomTab.Navigator>
   );
 }
 
-const getHeaderTitle = route => {
-  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+const getHeaderTitle = (route) => {
+  const routeName =
+    route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
-      return 'Home';
-    case 'Report':
-      return 'Report';
+    case "Home":
+      return "Home";
+    case "Report":
+      return "Report";
   }
-}
+};
